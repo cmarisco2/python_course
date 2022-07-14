10d1d76286af4c6d616b6fba6b7a99d5d22e2e6a
Christopher Marisco <christopher.marisco@gmail.com>
2022-07-14 15:09:28 -0600

    update with round() fn

--- mileage.py
+++ mileage.py
@@ -1,3 +1,8 @@
 print("How many kilometers did you run today?")
+# String from input()
 kms = input()
-print(f"Okay, you said {kms}")
+# convert string to float then divide
+miles = float(kms)/1.60934
+# round to 2 decimal places
+miles = round(miles, 2)
+print(f"Your {kms}km ride is {miles}miles")
