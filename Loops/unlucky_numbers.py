1e27f8f225d51f3685c19de14cd447d6e84c079d
Christopher Marisco <christopher.marisco@gmail.com>
2022-07-15 13:10:16 -0600

    simplify code to change variable and print as the last step of each loop

--- Loops/unlucky_numbers.py
+++ Loops/unlucky_numbers.py
@@ -6,8 +6,9 @@
 
 for num in range(1,21):
     if num == 4 or num == 13:
-        print(f"{num}: Unlucky")
+        state = "unlucky"
     elif num % 2 == 1:
-        print(f"{num}: Odd")
+        state = "Odd"
     else:
-        print(f"{num}: Even")
+        state = "Even"
+    print(f"{num}: {state}")
