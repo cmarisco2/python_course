242ea7fc79cab38004667e90fc1f3f92cea4637a
Christopher Marisco <christopher.marisco@gmail.com>
2022-07-15 12:51:24 -0600

    demo range basics

---
+++ Loops/range.py
@@ -0,0 +1,13 @@
+#demo range basics
+
+#Common Usage in For Loops
+for num in range(10):
+    print(num) #prints 1 - 9
+
+
+#Use to make list w/ intervals
+#Use with Step Arg (3rd arg)
+nums = range(10, 0, -1)
+nums = list(nums)
+
+print(nums)
