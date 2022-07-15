82035b2f057ffd2b67aad24aa54c069f121fb304
Christopher Marisco <christopher.marisco@gmail.com>
2022-07-15 13:16:46 -0600

    demo while loop on user input

---
+++ Loops/while.py
@@ -0,0 +1,8 @@
+#demo while loops
+
+msg = input("What's the secret code?: ")
+
+while msg != 'Logan Roy':
+    print("Incorrect, than answer is Logan Roy. Try again")
+    msg = input("What's the secret code?: ")
+print('Correct')
