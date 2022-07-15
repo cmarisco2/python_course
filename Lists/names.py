3aa46d00282dbdccf8a9d011580f5433df4131db
Christopher Marisco <christopher.marisco@gmail.com>
2022-07-15 17:03:40 -0600

    iterate over list w/ while loop

--- Lists/names.py
+++ Lists/names.py
@@ -1,6 +1,15 @@
-# demo iterating over list
+# demo iterating over list w/ for loop
 
 names = ["Eren", "Reiner", "Mikasa", "Armin", True, 100, 12.53]
 
 for word in names:
     print(word)
+
+
+# demo iterating list w/ while loop
+nums = [1, 2, 3]
+i = 0
+
+while i < len(nums):
+    print(nums[i] * nums[i])
+    i += 1
