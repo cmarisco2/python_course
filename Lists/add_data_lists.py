5c110dd2c176f48581046b7376e01e22ea735ce2
Christopher Marisco <christopher.marisco@gmail.com>
2022-07-15 17:26:51 -0600

    demo adding data via append(), extend(), and insert()

---
+++ Lists/add_data_lists.py
@@ -0,0 +1,15 @@
+# dummy list
+nums = [1, 2, 3]
+print(nums)
+
+# demo append
+nums.append("Hello, I'm new here!")
+print(nums)
+
+# demo extend
+nums.extend(["hi", 22.4, 17, False])
+print(nums)
+
+# demo insert
+nums.insert(0, len(nums))
+print(nums)
