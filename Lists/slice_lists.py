5387c58928d81b1b0f531fc4602444e67838eca5
Christopher Marisco <christopher.marisco@gmail.com>
2022-07-15 19:47:59 -0600

    display slicing features

---
+++ Lists/slice_lists.py
@@ -0,0 +1,16 @@
+# dummy list
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(nums)
+
+# slice -> list[start:end:step]
+my_list = nums[2:]
+print(my_list)
+
+# slice backwards
+my_list = nums[-5:]
+print(my_list)
+
+# slice w/ steps
+my_list = nums[3:9:2]
+print(my_list)
