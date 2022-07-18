272631d4ae93b28560720477af70fe6124a0a392
Christopher Marisco <christopher.marisco@gmail.com>
2022-07-18 14:01:08 -0600

    access dict values via string and variable

---
+++ Dictionaries/access_dict.py
@@ -0,0 +1,11 @@
+# access the key value pair via key
+
+cat = {"name": "blue", "age": 10, "isFurry": True}
+cat2 = dict(name="kitten", age='22')
+
+# print(f"cat1's name is: {cat["name"]}")
+print(cat["name"])
+
+name = "name"
+
+print(cat2[name])
