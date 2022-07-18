b9a3885a7ae5c1318655ae5a5809d5badd8390ac
Christopher Marisco <christopher.marisco@gmail.com>
2022-07-18 15:59:29 -0600

    add data to playlist

--- Dictionaries/SpotifyPlaylistModeling/playlist.py
+++ Dictionaries/SpotifyPlaylistModeling/playlist.py
@@ -1,1 +1,16 @@
 # demo that combines list and dictionary concepts into one small example
+
+# Playlist is just an obj/dict with key song-holding a list of songs
+
+# songs list will have objects/dicts that have song titles, artist, etc.
+# ie. each song is an object/dict
+
+playlist = {
+    'title': 'myFunk',
+    'author': 'Chris',
+    'song': [
+        {'title': 'song1', 'artist': ['blue'], 'duration': 2.5},
+        {'title': 'song2', 'artist': ['kitty', 'djCat'], 'duration': 3.5},
+        {'title': 'meowmeow', 'artist': ['Garfield'], 'duration': 2}
+    ]
+}
