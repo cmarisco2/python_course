cb32cf8c22b17e2bdf930b89a942d1dca8e2df9f
Christopher Marisco <christopher.marisco@gmail.com>
2022-07-18 12:07:42 -0600

    demo accessing and iterating over nested list

---
+++ ListComprehensions/nested_lists.py
@@ -0,0 +1,18 @@
+# demo accessing and iterating nested lists
+nested_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+# row major OR first indices id's list elem, 2nd id's elem in list
+
+print("Show entire list")
+print(nested_list)
+
+print(f"\nfirst list within the nested list is: {nested_list[0]}\n")
+print(f"Second elem of first list is: {nested_list[0][1]}")
+
+
+# iterate over list
+print("all elements of the list are:")
+
+for num_list in nested_list:
+    for val in num_list:
+        print(val)
