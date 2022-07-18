6f6d348980f0f8ed39532efa5b0af7741cea9190
Christopher Marisco <christopher.marisco@gmail.com>
2022-07-18 14:07:42 -0600

    initial commit iterating over dict values

---
+++ Dictionaries/iterate_dict.py
@@ -0,0 +1,10 @@
+# demo iterating over dictionary
+instructor = {
+    "name": "Chris",
+    "age": 32,
+    "isCool": True
+}
+
+# iterate over values:
+for val in instructor.values():
+    print(val)
