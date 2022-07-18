012caef2e28d7e18e05de0c34cb0a03ee40aa6b0
Christopher Marisco <christopher.marisco@gmail.com>
2022-07-18 12:41:06 -0600

    demo creating a dictionary explicit and using the dict() function

--- Dictionaries/create_dict.py
+++ Dictionaries/create_dict.py
@@ -1,4 +1,11 @@
 # demo dictionaries
-
+# explicit creation
 cat = {"name": "blue", "age": 10, "isFurry": True}
+
 print(f"cat dict is: {cat}")
+
+# create with dict()
+
+cat2 = dict(name="kitten", age='22')
+
+print(f"second cat is: {cat2}")
