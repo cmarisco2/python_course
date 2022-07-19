6403873640f7269033d78e1dcc6d04e3dd73afea
Christopher Marisco <christopher.marisco@gmail.com>
2022-07-19 12:59:40 -0600

    define and execute a function

---
+++ Functions/first_function.py
@@ -0,0 +1,9 @@
+# demo a basic function in python
+
+def sing_hhh_theme_():
+    # motorhead first several lines
+    print("IT's TIME TO PLAY THE GAME!!")
+    print("IT's TIME TO PLAY THE GAAMMMEEEE!!!")
+
+
+sing_hhh_theme_()
