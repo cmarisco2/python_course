80a9ba250ec5c0d15662e9e0f5d552e680a76072
Christopher Marisco <christopher.marisco@gmail.com>
2022-07-19 14:01:45 -0600

    demo using default args as well as keyword args in a funtion

--- Functions/param_args.py
+++ Functions/param_args.py
@@ -8,9 +8,16 @@ def multiply(var1, var2=2):
     return var1 * var2
 
 
+def exponent(base, power=2):
+    return base ** power
+
+
 print(say_hello('Janice'))
 
 print(multiply(3, 4))
 
 # default arg
 print(multiply(10))
+
+# keyword args
+print(exponent(power=3, base=4))
