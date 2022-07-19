9335c0ca832a27f08e8fceb054011937cdf0cd34
Christopher Marisco <christopher.marisco@gmail.com>
2022-07-19 13:46:06 -0600

    demo defining params and using arguments in python function

--- Functions/param_args.py
+++ Functions/param_args.py
@@ -4,10 +4,13 @@ def say_hello(name):
     return f"say hello to {name}"
 
 
-def multiply(var1, var2):
+def multiply(var1, var2=2):
     return var1 * var2
 
 
 print(say_hello('Janice'))
 
 print(multiply(3, 4))
+
+# default arg
+print(multiply(10))
