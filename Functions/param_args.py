22dcba22ff4bcea724529402b30615c7103f3310
Christopher Marisco <christopher.marisco@gmail.com>
2022-07-19 13:37:44 -0600

    demo defining params and using arguments in python function

---
+++ Functions/param_args.py
@@ -0,0 +1,13 @@
+# demo functions with parameters(in def) and arguments(in call)
+
+def say_hello(name):
+    return f"say hello to {name}"
+
+
+def multiply(var1, var2):
+    return var1 * var2
+
+
+print(say_hello('Janice'))
+
+print(multiply(3, 4))
