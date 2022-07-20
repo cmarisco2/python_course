02f19d7b7f2bcfde008e2f74bd9c151153fc7492
Christopher Marisco <christopher.marisco@gmail.com>
2022-07-20 13:10:05 -0600

    add another lambda example. print fn name and lambda names to display the anonymous feature of lambdas

--- Functions/Lambdas/first_lambda.py
+++ Functions/Lambdas/first_lambda.py
@@ -8,7 +8,14 @@ def square(num):
 # normal function call:
 print(f"normal fn call of square(4): {square(4)}")
 
-# lambda function called at creation:
+# lambda function stored in a variable:
 square2 = lambda num : num * num
+add = lambda a,b : a + b
 print(f"lambda function call of square(4): {square(4)}")
+print(add(2,3))
+
+# print to show lambda's are anonymous
+print(square.__name__)
+print(square2.__name__)
+print(add.__name__)
 
