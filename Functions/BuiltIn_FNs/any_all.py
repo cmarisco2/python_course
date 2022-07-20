cd93662e632202d1a7ee76befcadff98f325ae67
Christopher Marisco <christopher.marisco@gmail.com>
2022-07-20 15:46:02 -0600

    use any() and all() fns with list comprehension arguments

---
+++ Functions/BuiltIn_FNs/any_all.py
@@ -0,0 +1,8 @@
+# demo any and all fns
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# all(<iterable>)
+# passing list comprehension that returns list of bools rather than numbers
+print(f"are all numbers even? {all([num % 2 == 0 for num in numbers])}")
+print(f"are any numbers even? {any([num % 2 == 0 for num in numbers])}")
