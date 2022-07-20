51469e51bc5a246d9a3bcabfef572fadebd91bdc
Christopher Marisco <christopher.marisco@gmail.com>
2022-07-20 15:53:05 -0600

    use generator expression with the any() fn

--- Functions/BuiltIn_FNs/any_all.py
+++ Functions/BuiltIn_FNs/any_all.py
@@ -6,3 +6,9 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 # passing list comprehension that returns list of bools rather than numbers
 print(f"are all numbers even? {all([num % 2 == 0 for num in numbers])}")
 print(f"are any numbers even? {any([num % 2 == 0 for num in numbers])}")
+
+# Using Generator Expression (no need to return the whole list)
+# More lightweight
+print(
+    f"are any numbers even? (using gen exp) {any(num % 2 == 0 for num in numbers)}"
+)
