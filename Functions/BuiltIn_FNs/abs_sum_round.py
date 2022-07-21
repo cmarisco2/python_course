8628e843a616bdfb079bff04b4482839de7aac09
Christopher Marisco <christopher.marisco@gmail.com>
2022-07-21 15:21:21 -0600

    demo round()

--- Functions/BuiltIn_FNs/abs_sum_round.py
+++ Functions/BuiltIn_FNs/abs_sum_round.py
@@ -6,3 +6,9 @@ print(max(abs(x) for x in nums))
 
 print(f"sum of all the values in {nums} is: ")
 print(sum(x for x in nums))
+
+value = 3.14159265
+print(f"PI rounded to 8 sig figs: {round(value, 8)}")
+print(f"PI rounded to 4 sig figs: {round(value, 4)}")
+print(f"PI rounded to 2 sig figs: {round(value, 2)}")
+print(f"PI rounded as an integer: {round(value)}")
