99cb3796f32130c42ec91a9d666d6d1bb5c0d4cd
Christopher Marisco <christopher.marisco@gmail.com>
2022-07-21 15:17:21 -0600

    demo sum()

--- Functions/BuiltIn_FNs/abs_sum_round.py
+++ Functions/BuiltIn_FNs/abs_sum_round.py
@@ -3,3 +3,6 @@ nums = [2, 76, -11, 4, -3, -9000, 500]
 print(f"absolute value of the greatest magnitude in {nums} is: ")
 
 print(max(abs(x) for x in nums))
+
+print(f"sum of all the values in {nums} is: ")
+print(sum(x for x in nums))
