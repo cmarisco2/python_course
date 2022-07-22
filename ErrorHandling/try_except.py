786de8ee3266a90c926d87c0fa71f081613f151f
Christopher Marisco <christopher.marisco@gmail.com>
2022-07-22 13:15:20 -0600

    demo basic try and except features

--- ErrorHandling/try_except.py
+++ ErrorHandling/try_except.py
@@ -1,1 +1,25 @@
 # demo try/except
+
+import errno
+
+
+try:
+    foobar
+except:
+    print('Some error happend')
+print('But we continued')
+
+# demo catching a specific error:
+
+d = {'name': 'bobby'}
+
+
+def get(dictionary, key):
+    try:
+        dictionary[key]
+    except KeyError:
+        return None
+
+
+print(d.get('name'))
+print(d.get('city'))
