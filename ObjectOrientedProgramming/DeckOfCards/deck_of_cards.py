fe4eb9fd49946ac98819d53c58437253d8dea516
Christopher Marisco <christopher.marisco@gmail.com>
2022-07-25 12:51:41 -0600

    implement deck api and test

--- ObjectOrientedProgramming/DeckOfCards/deck_of_cards.py
+++ ObjectOrientedProgramming/DeckOfCards/deck_of_cards.py
@@ -1,4 +1,6 @@
 # build a card class as specified on Udemy 'Modern Python3 Bootcamp' Lec 25:
+from random import shuffle
+
 
 class Card:
 
@@ -34,5 +36,38 @@ class Deck:
                   '8', '9', '10', 'J', 'Q', 'K')
         self.cards = [Card(value, suit) for suit in suits for value in values]
 
+    def __repr__(self):
+        return f"Deck of {self.count()} cards"
 
-Deck()
+    def count(self):
+        return len(self.cards)
+
+    def _deal(self, num):
+        count = self.count()
+        actual = min(count, num)
+        if count == 0:
+            raise ValueError('All cards have been dealt')
+        cards = self.cards[-actual:]
+        self.cards = self.cards[:-actual]
+        return cards
+
+    def deal_card(self):
+        return self._deal(1)[0]
+
+    def deal_hand(self, hand_size):
+        return self._deal(hand_size)
+
+    def shuffle(self):
+        if self.count() < 52:
+            raise ValueError('Only full decks can be shuffled')
+        shuffle(self.cards)
+        return self
+
+
+d = Deck()
+d.shuffle()
+card = d.deal_card()
+print(card)
+hand = d.deal_hand(5)
+print(hand)
+print(d.count())
