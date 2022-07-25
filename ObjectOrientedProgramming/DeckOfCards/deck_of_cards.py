f6f05166b09ed458171e10a44a248cd56839f798
Christopher Marisco <christopher.marisco@gmail.com>
2022-07-25 12:56:12 -0600

    remove redundant code by referencing Card class attributes

--- ObjectOrientedProgramming/DeckOfCards/deck_of_cards.py
+++ ObjectOrientedProgramming/DeckOfCards/deck_of_cards.py
@@ -31,10 +31,10 @@ class Card:
 class Deck:
 
     def __init__(self):
-        suits = ('Hearts', 'Diamonds', 'Clubs', 'Spades')
-        values = ('A', '2', '3', '4', '5', '6', '7',
-                  '8', '9', '10', 'J', 'Q', 'K')
-        self.cards = [Card(value, suit) for suit in suits for value in values]
+        self.cards = [
+            Card(value, suit)
+            for suit in Card.valid_suits for value in Card.valid_values
+        ]
 
     def __repr__(self):
         return f"Deck of {self.count()} cards"
