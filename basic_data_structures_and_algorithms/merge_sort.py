db9039553ceacc67505f097561a87e0dd5fb2497
Christopher Marisco <christopher.marisco@gmail.com>
2022-08-14 18:57:38 -0600

    add clarifying comments

--- basic_data_structures_and_algorithms/merge_sort.py
+++ basic_data_structures_and_algorithms/merge_sort.py
@@ -1,28 +1,39 @@
+
+# Big Help to fix bugs from link below!
 # https: // github.com/chen0040/pyalgs/blob/master/pyalgs/algorithms/commons/sorting.py
 
 # define merge and merge sort
-from random import shuffle
-nums = [5, 9, 11, 3, 4, 6]
+import random
 
-shuffle(nums)
-print(nums)
 
-# array partitioned by 2 sub-arrays of equal length.
-# the result is a single merged array in ascending order
+"""
+    Merge function requires 3 key pieces of attention
+    1) hi -> passed in as len(arr) - 1
+        -Need hi + 1 in the range to include all indices
+        -Means only for loop needs adjusting but all else is normal
+    2) cpy arr in this function 
+        -Ensures an update to the moved values occurs in cpy
+    3) j = mid + 1
+        -Ensures correct index position for the merging arrays
+    4) i > mid AND j > hi
+        -Ensure no funky indice errors on merging
+"""
 
 
 def _merge(arr, lo, mid, hi):
-
-    # copy the array to the aux array in order
-    aux = arr[::1]
-
     i = lo
     j = mid + 1
 
+    # copy the array to the aux array in order
+    aux = arr[::]
+
+    # ensure range is hi + 1 (len()-1 is passed, need hi to be inclusive)
     for k in range(lo, hi + 1):
+        # range check is GREATER than MID
         if i > mid:
             arr[k] = aux[j]
             j += 1
+        # range check is GREATER than HI
         elif j > hi:
             arr[k] = aux[i]
             i += 1
@@ -33,22 +44,26 @@ def _merge(arr, lo, mid, hi):
             arr[k] = aux[j]
             j += 1
 
+# recursive function built well!
+
 
 def _sort(arr, lo, hi):
     if hi <= lo:
         return
     mid = lo + (hi - lo) // 2
-    print(mid)
-    _sort(arr,  lo, mid)
-    _sort(arr,   mid + 1, hi)
-    _merge(arr,  lo, mid, hi)
+    _sort(arr, lo, mid)
+    _sort(arr, mid + 1, hi)
+    _merge(arr, lo, mid, hi)
 
 
 def mergeSort(arr):
-    _sort(arr,  0, len(arr) - 1)
+    _sort(arr, 0, len(arr) - 1)
 
 
-# aux = nums[::1]
-# _merge(nums, aux, 0, len(nums)//2, len(aux))
+nums = list(range(random.randint(0, 20)))
+random.shuffle(nums)
+
+print(nums)
+
 mergeSort(nums)
 print(nums)
