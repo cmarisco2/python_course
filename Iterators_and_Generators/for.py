690219a0e6a16d9b8e210fc02a5d62806dcd2e8c
Christopher Marisco <christopher.marisco@gmail.com>
2022-09-22 14:21:37 -0600

    write custom for loop using iter() and next() functions while catching StopIteration Error to break the loop

--- Iterators_and_Generators/for.py
+++ Iterators_and_Generators/for.py
@@ -1,6 +1,30 @@
-# Custom For Loop
+# Custom For Loop:
+# Get an iterator via iter()
+# While No StopIteration Error is Raised:
+# next(iterator)
 
 
-#print each element in iterable objects
-def my_for(iterable):
-    it = iter(iterable)
+# Print each element in iterable objects
+def my_for(iterable, func):
+    # Grabs iterator via iter() on an iterable
+    iterator = iter(iterable)
+    # Use while loop until Stop Iteration Error
+    while True:
+        try:
+            # Store each element via next()
+            thing = next(iterator)
+        except StopIteration:
+            # Need break statement else -> INFINITE LOOP
+            break
+        else:
+            # Note thing is one element of the iterator object
+            func(thing)
+
+
+def square(x):
+    print(x*x)
+
+
+my_for("HELLO", print)
+print()
+my_for([1, 2, 3, 4], square)
