de288aaa184489210a9f9574ea0918d162b0fc63
Christopher Marisco <christopher.marisco@gmail.com>
2022-10-19 19:27:16 -0600

    create REPL doctest

---
+++ Tests/doctest_demo.py
@@ -0,0 +1,16 @@
+# use REPL syntax to write tests
+# run via cli -> python3 -m doctest -v <filename>
+
+
+def add_nums(a, b):
+    """
+    adds numbers a & b
+    >>> add_nums(2,3)
+    5
+
+    >>> add_nums(100,200)
+    300
+
+    """
+
+    return a + b
