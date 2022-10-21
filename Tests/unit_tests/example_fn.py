a1d8a337cd0f188687bb4531ea9eb809bd60d668
Christopher Marisco <christopher.marisco@gmail.com>
2022-10-20 18:26:07 -0600

    add test cases

--- Tests/unit_tests/example_fn.py
+++ Tests/unit_tests/example_fn.py
@@ -2,6 +2,12 @@ def add_nums(a, b):
     return a + b
 
 
+def is_funny(person):
+    if person == 'Tim':
+        return False
+    return True
+
+
 if __name__ == "__main__":
     demo = add_nums(1, 5)
     print(demo)
--- Tests/unit_tests/unit_tests.py
+++ Tests/unit_tests/unit_tests.py
@@ -2,7 +2,7 @@
 
 # 1 -> import unittest and the module/class/fns to be tested
 import unittest
-from example_fn import add_nums
+from example_fn import add_nums, is_funny
 
 # 2 -> Make class that inherits from unittest.TestCase
 # 3 -> Define each method to begin with test* & add assert property to the self instance
@@ -16,6 +16,16 @@ class ExampleTester(unittest.TestCase):
     def test_add_wrong(self):
         """Ensure value does not equal certain value"""
         self.assertNotEqual(add_nums(2, 3), 0)
+
+    def test_is_funny(self):
+        """Ensure John, Jack, Jim are funny"""
+        self.assertTrue(is_funny("John"), "Should be Funny")
+        self.assertTrue(is_funny("Jack"), "Should be Funny")
+        self.assertTrue(is_funny("Jim"), "Should be Funny")
+
+    def test_is_not_funny(self):
+        """Ensure that Tim is Not Funny!"""
+        self.assertFalse(is_funny('Tim'), "Should Not be funny")
 
 
 # 4 -> run unittest.main() only if the test function is run
